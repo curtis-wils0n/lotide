bb25d6be7cf65979409cf8106daf320d7a3c0a5e
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-09-19 21:28:59 +0000

    Completed assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,14 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  
+  if (!(actual === expected)) {
+    console.log("🛑🛑🛑 Assertion failed: (" + actual + " !== " + expected + ")");
+    return false;
+  } else {
+    console.log("✅✅✅ Assertion passed: (" + actual + " === " + expected + ")");
+    return true;
+  }
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 1);
