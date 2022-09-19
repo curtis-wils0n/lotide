ef8b65de236b04506862ab121ef100ed577d658a
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-09-19 21:34:25 +0000

    Improved assertEqual.js using Template Literals for readability.

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,14 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (!(actual === expected)) {
-    console.log("🛑🛑🛑 Assertion failed: (" + actual + " !== " + expected + ")");
+    console.log(`🛑🛑🛑 Assertion failed: (${actual} !== ${expected})`);
     return false;
   } else {
-    console.log("✅✅✅ Assertion passed: (" + actual + " === " + expected + ")");
+    console.log(`✅✅✅ Assertion passed: (${actual} === ${expected})`);
     return true;
   }
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Lobs", "Lighthouse Labs");
 assertEqual(1, 1);
