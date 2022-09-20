8c9a4a671807a112802f0c84821c5a7644d2c535
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-09-20 21:34:02 +0000

    Removed assertEqual function from eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (!(actual === expected)) {
-    console.log(`🛑🛑🛑 Assertion failed: (${actual} !== ${expected})`);
-    return false;
-  } else {
-    console.log(`✅✅✅ Assertion passed: (${actual} === ${expected})`);
-    return true;
-  }
-};
-
 const eqArrays = function(array1, array2) {
   let resultFlag = true;
   for (let i = 0; i < array1.length; i++) {
@@ -18,5 +8,3 @@ const eqArrays = function(array1, array2) {
   }
   return resultFlag;
 };
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
