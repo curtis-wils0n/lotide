31c5209f2d35f860d9942b9168a5f441ac671cf6
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-09-21 20:24:27 +0000

    Completed countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,17 @@
+// allItems: an array of string that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    console.log(item);
+    if (itemsToCount[item]) {  
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
