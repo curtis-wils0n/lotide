865ac1a63b5580a8cc5b730b3d04f9fbaa023489
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-09-21 16:47:40 +0000

    Added middle function implementation

---
+++ middle.js
@@ -0,0 +1,10 @@
+const middle = function(array) {
+  let outputArray = [];
+  let index = parseInt(array.length / 2);
+  if (array.length % 2 === 0 && array.length > 2) {
+    outputArray.push(array[index - 1], array[index]);
+  } else if (array.length > 2) {
+    outputArray.push(array[index]);
+  }
+  return outputArray;
+};
